9f79a7d460238707e36788abf1cbe512c2f2c491
Cristian Graziano <cristian.graziano@gmail.com>
2016-01-06 00:15:54 -0700

    Validator: Rename schema to validator. Move from GoLoader to StringLoader. GoLoader validates the struct, so when passing a struct to NewGoLoader the entire struct is validated against the schema rather than only validating the specific fields sent in the request. The StringLoader ensures that the schema is validated only against the request body

--- api/validator/schema.go
+++ api/validator/schema.go
@@ -1,6 +1,7 @@
 package validator
 
 import (
+	"bytes"
 	"encoding/json"
 	"io"
 	"net/http"
@@ -9,56 +10,60 @@ import (
 	"github.com/xeipuuv/gojsonschema"
 )
 
-// Schema is a JSON schema and validator. It holds a json schema,
+// Validator is a JSON schema and validator. It holds a json schema,
 // pointer to a Validator, and optional limit for an io.LimitReader.
-type Schema struct {
+type Validator struct {
 	schema  gojsonschema.JSONLoader
 	options *Options
 	limit   int64
 }
 
-// NewSchema returns a new Schema. If limit > 0, the limit overwrites
+// NewValidator returns a new Validator. If limit > 0, the limit overwrites
 // the limit set in the Validator.
-func NewSchema(schema gojsonschema.JSONLoader, options *Options, limit int64) Schema {
-	return Schema{
+func NewValidator(schema gojsonschema.JSONLoader, options *Options, limit int64) Validator {
+	return Validator{
 		schema:  schema,
 		options: options,
 		limit:   limit,
 	}
 }
 
-// Valid validates a schema against a validator and handles error responses.
+// Valid validates a request against a json schema and handles error responses.
 // If the response is successful the dst struct will be populated.
-func (s Schema) Valid(dst interface{}, w http.ResponseWriter, r *http.Request) bool {
+func (v Validator) Valid(dst interface{}, w http.ResponseWriter, r *http.Request) bool {
 	var reader io.Reader = r.Body
-	if s.limit > 0 {
-		reader = io.LimitReader(reader, s.limit)
-	} else if s.options.Limit > 0 {
-		reader = io.LimitReader(reader, s.options.Limit)
+	if v.limit > 0 {
+		reader = io.LimitReader(reader, v.limit)
+	} else if v.options.Limit > 0 {
+		reader = io.LimitReader(reader, v.options.Limit)
 	}
-	err := json.NewDecoder(reader).Decode(&dst)
+
+	b := new(bytes.Buffer)
+	tee := io.TeeReader(reader, b)
+	err := json.NewDecoder(tee).Decode(&dst)
 	defer r.Body.Close()
 	if err != nil {
 		switch err {
 		case io.EOF:
 			// Empty body
-			s.options.RequestBodyRequired.SendFormat(w, s.options.Formatter)
+			v.options.RequestBodyRequired.SendFormat(w, v.options.Formatter)
 			return false
 		default:
-			s.options.InvalidJSON.SendFormat(w, s.options.Formatter)
+			v.options.InvalidJSON.SendFormat(w, v.options.Formatter)
 			return false
 		}
 	}
 
-	document := gojsonschema.NewGoLoader(dst)
-	result, err := gojsonschema.Validate(s.schema, document)
+	document := gojsonschema.NewStringLoader(string(b.Bytes()))
+	result, err := gojsonschema.Validate(v.schema, document)
 	if err != nil {
 		switch err.(type) {
 		case *json.SyntaxError:
-			s.options.InvalidJSON.SendFormat(w, s.options.Formatter)
+			v.options.InvalidJSON.SendFormat(w, v.options.Formatter)
 			return false
 		default:
-			s.options.BadRequest.SendFormat(w, s.options.Formatter)
+			// This could mean there is an error processing your schema.
+			v.options.BadRequest.SendFormat(w, v.options.Formatter)
 			return false
 		}
 	}
@@ -67,13 +72,13 @@ func (s Schema) Valid(dst interface{}, w http.ResponseWriter, r *http.Request) b
 		return true
 	}
 
-	e := Swap(result.Errors(), s.options.Rules)
+	e := Swap(result.Errors(), v.options.Rules)
 
 	statusCode := 422
-	if s.options.ErrorStatus > 0 {
-		statusCode = s.options.ErrorStatus
+	if v.options.ErrorStatus > 0 {
+		statusCode = v.options.ErrorStatus
 	}
-	api.ErrorResponse(statusCode, e...).SendFormat(w, s.options.Formatter)
+	api.ErrorResponse(statusCode, e...).SendFormat(w, v.options.Formatter)
 
 	return false
 }
