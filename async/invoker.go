afec2531580e0be39cb44f614c7c4dc64b5912a1
Cristian Graziano <cristian.graziano@gmail.com>
2016-10-06 21:06:27 -0700

    Remove the async package

--- async/invoker.go
+++
@@ -1,8 +0,0 @@
-package async
-
-import "github.com/cristiangraz/kumi/api"
-
-// Invoker is used to invoke async methods.
-type Invoker interface {
-	Invoke(name string, msg *Message, async bool) (*api.Response, error)
-}
